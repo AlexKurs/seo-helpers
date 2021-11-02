93383cd2a7af2da392d1dde603dcb60fed342ca7
AlexKurs <kurshalow@gmail.com>
2021-11-02 12:37:33 +0100

    fixed bugs

--- meta_description_generator.py
+++ meta_description_generator.py
@@ -5,6 +5,7 @@ count = int(input('Anzahl: '))
 
 meta = ['TOP-Auswahl!', '★ TOP Qualität', 'Super Service ☆', '✓ hier klicken ▶ und mehr erfahren']
 
+
 for daten in range(count):
     meta.append(key)
     random.shuffle(meta)
@@ -15,3 +16,5 @@ for daten in range(count):
         print(result)
     else:
         continue
+
+print('Finish')
